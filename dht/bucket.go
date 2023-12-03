35e66086edc296c99f2a14f053ee13dbc328c9a6
André Moraes <andrelom@gmail.com>
2023-12-03 18:02:18 -0300

    Update bucket.go

--- dht/bucket.go
+++ dht/bucket.go
@@ -2,8 +2,31 @@ package dht
 
 import "math/big"
 
+const K = 20
+const B = 20
+
 type Bucket struct {
 	lower, upper *big.Int
 	nodes        []Node
 	replacements []Node
+}
+
+func NewBucket() *Bucket {
+	lower := big.NewInt(0)
+	upper := new(big.Int).Exp(big.NewInt(2), big.NewInt(B*8), nil)
+	return &Bucket{
+		lower:        lower,
+		upper:        upper,
+		nodes:        make([]Node, 0),
+		replacements: make([]Node, 0),
+	}
+}
+
+func NewBucketWithRange(lower, upper *big.Int) *Bucket {
+	return &Bucket{
+		lower:        lower,
+		upper:        upper,
+		nodes:        make([]Node, 0),
+		replacements: make([]Node, 0),
+	}
 }
