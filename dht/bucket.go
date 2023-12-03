1bf454890ec477da5d51c98f516e777a2b91f468
André Moraes <andrelom@gmail.com>
2023-12-03 20:55:40 -0300

    Update bucket.go

--- dht/bucket.go
+++ dht/bucket.go
@@ -31,18 +31,18 @@ func NewBucketWithRange(lower, upper *big.Int) *Bucket {
 func (buc *Bucket) Depth() int {
 	values := make([]string, len(buc.nodes))
 	for i, node := range buc.nodes {
-		values[i] = node.Key.Text(2)
+		values[i] = node.Key.ToBigInt().Text(2)
 	}
 	shared := getSharedPrefix(values)
 	return len(shared)
 }
 
-func (buc *Bucket) Fits(key *big.Int) bool {
-	return key.Cmp(buc.upper) < 0
+func (buc *Bucket) Fits(key *Key) bool {
+	return key.ToBigInt().Cmp(buc.upper) < 0
 }
 
-func (buc *Bucket) FitsInRange(key *big.Int) bool {
-	return key.Cmp(buc.lower) >= 0 && buc.upper.Cmp(key) >= 0
+func (buc *Bucket) FitsInRange(key *Key) bool {
+	return key.ToBigInt().Cmp(buc.lower) >= 0 && buc.upper.Cmp(key.ToBigInt()) >= 0
 }
 
 func (buc *Bucket) Add(node *Node) bool {
@@ -65,7 +65,7 @@ func (buc *Bucket) Remove(node *Node) bool {
 	return remove(&buc.replacements, node) || removed
 }
 
-func (buc *Bucket) FindNodeByKey(key *big.Int) *Node {
+func (buc *Bucket) FindNodeByKey(key *Key) *Node {
 	return getNodeByKey(buc.nodes, key)
 }
 
@@ -76,7 +76,7 @@ func (buc *Bucket) Split() (*Bucket, *Bucket) {
 	lowerBucket := NewBucketWithRange(buc.lower, middle)
 	upperBucket := NewBucketWithRange(new(big.Int).Add(middle, big.NewInt(1)), buc.upper)
 	for _, node := range nodes {
-		if node.Key.Cmp(middle) <= 0 {
+		if node.Key.ToBigInt().Cmp(middle) <= 0 {
 			lowerBucket.Add(node)
 		} else {
 			upperBucket.Add(node)
@@ -123,9 +123,9 @@ func getSharedPrefix(values []string) string {
 	return prefix
 }
 
-func getNodeByKey(nodes []*Node, key *big.Int) *Node {
+func getNodeByKey(nodes []*Node, key *Key) *Node {
 	for _, node := range nodes {
-		if node.Key.Cmp(key) == 0 {
+		if node.Key.ToBigInt().Cmp(key.ToBigInt()) == 0 {
 			return node
 		}
 	}
@@ -137,7 +137,7 @@ func toTailIfExists(nodes *[]*Node, node *Node) bool {
 		return false
 	}
 	for idx, val := range *nodes {
-		if val.Key.Cmp(node.Key) == 0 {
+		if val.Key.ToBigInt().Cmp(node.Key.ToBigInt()) == 0 {
 			*nodes = append((*nodes)[:idx], (*nodes)[idx+1:]...)
 			*nodes = append(*nodes, node)
 			return true
@@ -148,7 +148,7 @@ func toTailIfExists(nodes *[]*Node, node *Node) bool {
 
 func remove(nodes *[]*Node, target *Node) bool {
 	for idx, node := range *nodes {
-		if node.Key.Cmp(target.Key) == 0 {
+		if node.Key.ToBigInt().Cmp(target.Key.ToBigInt()) == 0 {
 			*nodes = append((*nodes)[:idx], (*nodes)[idx+1:]...)
 			return true
 		}
